04481221a0588c8475d071b28d927a17fa3c09c6
Aleksandr Toropov <toropov280304@icloud.com>
2024-03-08 22:32:17 +0300

    add layout for launch screen and main screen

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black (iOS)") ?? .black }
+    static var ypWhite: UIColor { UIColor(named: "YP White (iOS)") ?? .white }
+    static var ypBackground: UIColor { UIColor(named: "YP Background (iOS)") ?? .darkGray }
+    static var ypRed: UIColor { UIColor(named: "YP Red (iOS)") ?? .red }
+    static var ypGreen: UIColor { UIColor(named: "YP Green (iOS)") ?? .green }
+    static var ypGray: UIColor { UIColor(named: "YP Gray (iOS)") ?? .lightGray }
+}
